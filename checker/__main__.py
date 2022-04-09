1e4f30457db15db928587d68e880ccf40585ca32
Home <faridarab4218@gmail.com>
2022-04-09 23:09:02 +0430

    Add site_is_online method.

---
+++ checker/__main__.py
@@ -0,0 +1,37 @@
+from urllib.request import urlopen
+from http.client import HTTPConnection
+from urllib.parse import urlparse
+
+
+def site_is_online(url: str, timeout: int = 2) -> bool:
+    """
+    Return True if the target URL is online.
+    Raise an exception
+    :param url:
+    :param timeout:
+    :return:
+    """
+    error = Exception('unknown error')
+    parser = urlparse(url)
+    host = parser.netloc or parser.path.split('/')[0]
+    for port in (80, 443):
+        connection = HTTPConnection(host=host, port=port, timeout=timeout)
+        try:
+            connection.request('HEAD', '/')
+            return True
+        except Exception as ex:
+            error = ex
+        finally:
+            connection.close()
+    raise error
+
+
+if __name__ == '__main__':
+    pass
+    # response = urlopen('https://python.org')
+    # print(response.read())
+    # connection = HTTPConnection('varzesh3.com', port=80, timeout=10)
+    # connection.request("HEAD", '/')
+    #
+    # response = connection.getresponse()
+    # print(response.getheaders())
